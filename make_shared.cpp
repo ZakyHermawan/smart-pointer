acc3fe4ad633c69b7e77a6c439a3e0bc71240872
ZakyHermawan <zaky.hermawan9615@gmail.com>
2021-02-07 04:39:12 +0700

    Add make_shared

---
+++ make_shared.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <memory>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+class Resource
+{
+public:
+    Resource() {
+        cout << "Resource Acquired" << endl;
+    }
+    ~Resource() {
+        cout << "Resource Destroyed" << endl;
+    }
+    void hello() { cout << "hello" << endl; }
+};
+
+int main() {
+    std::shared_ptr<Resource> ptr(std::make_shared<Resource>());
+    {
+        std::shared_ptr ptr2(ptr);
+        cout << "kill one shared_ptr" << endl;
+    }
+    cout << "kill another shared_ptr" << endl;
+
+
+
+    return 0;
+}
